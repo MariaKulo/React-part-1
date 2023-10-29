a169c583cb36b95f6d8afd323077fc6e1b96ba0c
Maria Kulo <mariavkulo@gmail.com>
2023-10-29 14:57:22 +0200

    Re-rendering

--- counter/src/main.jsx
+++ counter/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App'
+
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const refresh = () => {
+  root.render(<App counter={counter} />)
+}
+
+refresh()
+counter+=1
+refresh()
+counter+=1
+refresh()
