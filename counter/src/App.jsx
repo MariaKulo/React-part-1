39115d4a222356cca0da01f96b78155394f7b293
Maria Kulo <mariavkulo@gmail.com>
2023-10-30 06:51:06 +0200

    Changes in state cause rerendering

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -8,7 +8,7 @@ const Display = (props) => {
 
 const Button = (props) => {
   return (
-    <button onClick={props.handleCLick}>
+    <button onClick={props.handleClick}>
       {props.text}
     </button>
   )
@@ -17,10 +17,16 @@ const Button = (props) => {
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
   const increaseByOne = () => setCounter(counter + 1)
+    console.log('increasing, value before', counter)
+
   const decreaseByOne = () => setCounter(counter - 1)
+    console.log('decreasing, value before', counter)
+
   const setToZero = () => setCounter(0)
+    console.log('resetting to zero, value before', counter)
 
   return (
     <div>
