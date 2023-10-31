620f20d0dcb747ff6965f94b1cbf47a3216f8606
Maria Kulo <mariavkulo@gmail.com>
2023-10-31 08:44:32 +0200

    Refactored components

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
+const Button = ({ handleClick, text }) => {
   return (
-    <button onClick={props.handleClick}>
-      {props.text}
+    <button onClick={handleClick}>
+      {text}
     </button>
   )
 }
@@ -19,14 +15,17 @@ const App = () => {
   const [ counter, setCounter ] = useState(0)
   console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter + 1)
+  const increaseByOne = () => {setCounter(counter + 1)
     console.log('increasing, value before', counter)
+  }
 
-  const decreaseByOne = () => setCounter(counter - 1)
+  const decreaseByOne = () => {setCounter(counter - 1)
     console.log('decreasing, value before', counter)
+  }
 
-  const setToZero = () => setCounter(0)
+  const setToZero = () => {setCounter(0)
     console.log('resetting to zero, value before', counter)
+  }
 
   return (
     <div>
