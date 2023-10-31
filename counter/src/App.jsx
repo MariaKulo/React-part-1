8df8c8760965ed82267fcd19fe82fd09fe1edce2
Maria Kulo <mariavkulo@gmail.com>
2023-10-31 14:25:46 +0200

    A more complex state: left and right clicks

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,44 +1,35 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
 const App = () => {
+  const [clicks, setClicks] = useState({ 
+    left: 0, right: 0
+  })
 
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {setCounter(counter + 1)
-    console.log('increasing, value before', counter)
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      right: clicks.right
+    }
+    setClicks(newClicks)
   }
 
-  const decreaseByOne = () => {setCounter(counter - 1)
-    console.log('decreasing, value before', counter)
+  const handleRightClick = () => {
+    const newClicks = {
+      left: clicks.left,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
   }
 
-  const setToZero = () => {setCounter(0)
-    console.log('resetting to zero, value before', counter)
-  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button handleClick={increaseByOne}
-      text='plus'
-      />
-      <Button handleClick={setToZero}
-      text='zero'
-      />
-      <Button handleClick={decreaseByOne}
-      text='minus'
-    />
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
     </div>
   )
 }
